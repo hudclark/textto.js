02cf6caec4489e0f183bf8570cc8e59c1e13fe17
hudclark <hudclark@umich.edu>
2017-08-14 20:11:02 -0400

    Add loading-view component

--- app/components/message-view/component.js
+++ app/components/message-view/component.js
@@ -12,6 +12,12 @@ export default Ember.Component.extend({
     received: Ember.computed('message', function () {
         let message = this.get('message');
         return (message.sender !== 'me');
+    }),
+
+    isLoading: Ember.computed('message', function () {
+        if (!this.get('isScheduled')) return false
+        const message = this.get('message')
+        return (!message.sent || !message.failed)
     })
 
 });
