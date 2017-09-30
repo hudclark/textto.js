e6c5977acc2e1325743ca71ca8cb4bde9fda4aea
hudclark <hudclark@umich.edu>
2017-09-30 15:41:48 -0400

    Protect emoji text from xss

--- app/components/emoji-text/component.js
+++ app/components/emoji-text/component.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-    
-
     text: Ember.computed('content', function() {
         if (this.get('content')) {
-            return twemoji.parse(this.get('content'));
+            const dirty = this.get('content')
+            const clean = DOMPurify.sanitize(dirty)
+            console.log(clean)
+            return twemoji.parse(clean);
         } else {
             return "";
         }
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,6 +14,8 @@ module.exports = function(defaults) {
     }
   });
 
+  app.import('vendor/purify.js')
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
