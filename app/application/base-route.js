857e8ca89b6098ea980262668ddedef8f16c4102
hudclark <hudclark@umich.edu>
2018-01-13 21:48:40 -0500

    Change page description

--- app/application/base-route.js
+++ app/application/base-route.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
 
     setMeta: Ember.on('activate', function () {
         const title = this.title || 'Textto | Text From Your Computer'
-        const description = this.description || 'Sync your Android device with Textto and text for free with the website and cross platform desktop applications.'
+        const description = this.description || 'Sync your Android device with Textto and text from your computer for free!'
         document.title = title
         $('meta[name=description]').attr('content', description)
     }),
