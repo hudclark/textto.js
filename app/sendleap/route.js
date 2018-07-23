f78372a6ca33fcc41ad9c18581660276da71ca25
hudclark <hudclark@umich.edu>
2018-07-22 21:05:54 -0500

    Add sendleap page

--- app/sendleap/route.js
+++ app/sendleap/route.js
@@ -2,6 +2,6 @@ import BaseRoute from 'textto/application/base-route'
 
 export default BaseRoute.extend({
 
-    title: 'End-To-End Encryption | SendLeap'
+    title: 'Textto becomes SendLeap | SendLeap'
 
 })
